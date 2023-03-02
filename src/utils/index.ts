dcc13785a81fe65c25795b542f64ca181e4651b2
AntohaBr <brelanton@gmail.com>
2023-03-02 16:22:44 +0300

    fix: start absolute imports

---
+++ src/utils/index.ts
@@ -0,0 +1,7 @@
+export * from './Local-storage'
+export * from './Hooks'
+export {validate} from './Validate'
+export {useDebounce} from './Use-debounce'
+export {error} from './Error'
+export {getCard} from './Smart-random'
+export {convertFileToBase64} from './Convert-fаile-to-base64'
