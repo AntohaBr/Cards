1fcc619620cc405c2818f43954f879621b9950ec
AntohaBr <brelanton@gmail.com>
2023-05-19 10:34:22 +0300

    feat(learn): add Responsive design

--- src/feautures/Learn/Learn.tsx
+++ src/feautures/Learn/Learn.tsx
@@ -13,130 +13,128 @@ import t from 'common/Styles/Table.module.css'
 const grades = ['I did not know', 'I forgot', 'I thought for a long time', 'I got confused', 'I knew the answer']
 
 
-export const Learn = memo (() => {
-    const packName = useAppSelector(selectCardsPackName)
-    const cards = useAppSelector(selectCards)
+export const Learn = memo(() => {
+        const packName = useAppSelector(selectCardsPackName)
+        const cards = useAppSelector(selectCards)
 
-    const dispatch = useAppDispatch()
-    const {packId} = useParams<'packId'>()
+        const dispatch = useAppDispatch()
+        const {packId} = useParams<'packId'>()
 
-    const [answer, setAnswer] = useState<boolean>(false)
-    const [value, setValue] = useState<number>(0)
-    const [card, setCard] = useState<CardType>({
-        _id: '',
-        cardsPack_id: '',
-        user_id: '',
-        answer: '',
-        question: '',
-        answerImg: '',
-        questionImg: '',
-        grade: 0,
-        shots: 0,
-        comments: '',
-        type: '',
-        rating: 0,
-        more_id: '',
-        created: '',
-        updated: '',
-        __v: 0,
-    })
+        const [answer, setAnswer] = useState<boolean>(false)
+        const [value, setValue] = useState<number>(0)
+        const [card, setCard] = useState<CardType>({
+            _id: '',
+            cardsPack_id: '',
+            user_id: '',
+            answer: '',
+            question: '',
+            answerImg: '',
+            questionImg: '',
+            grade: 0,
+            shots: 0,
+            comments: '',
+            type: '',
+            rating: 0,
+            more_id: '',
+            created: '',
+            updated: '',
+            __v: 0,
+        })
 
-    const onClickButtonHandler = () => {
-        setAnswer(!answer)
-    }
-
-    const onNext = () => {
-        setAnswer(false)
-        setValue (0)
-        if (cards.length > 0) {
-            dispatch(
-                updateGradeCard({card_id: card._id, grade: value, shots: card.shots})
-            )
-            setCard(getCard(cards))
-        }
-    }
-
-    useEffect(() => {
-        if (!packName) {
-            dispatch(getCards({cardsPack_id: packId}))
+        const onClickButtonHandler = () => {
+            setAnswer(!answer)
         }
 
-        if (cards.length > 0) {
-            setCard(getCard(cards))
+        const onNext = () => {
+            setAnswer(false)
+            setValue(0)
+            if (cards.length > 0) {
+                dispatch(
+                    updateGradeCard({card_id: card._id, grade: value, shots: card.shots})
+                )
+                setCard(getCard(cards))
+            }
         }
 
-        return () => {
-        }
-    }, [dispatch, packId, cards])
+        useEffect(() => {
+            if (!packName) {
+                dispatch(getCards({cardsPack_id: packId}))
+            }
 
-    return (
-        <div className={f.container}>
-            <BackToPackList/>
-            <div>
-                <div className={t.titlePack}>Learn: {`'${packName}'`}</div>
-                <div className={s.learnBlock}>
-                    <div className={s.titleSection}>Question:</div>
-                    <div className={s.QuestionAnswerBlock}>
-                        {card.questionImg
-                            ?
-                            <img alt={'question image'}
-                                 src={card.questionImg}
-                                 style={{maxWidth: '375px', maxHeight: '120px', marginTop: '20px'}}/>
-                            :
-                            <div>{card.question}</div>
-                        }
-                    </div>
-                    <div className={f.text}>Number of attempts to answer the question: {card.shots}</div>
-                    <Button variant={'contained'}
-                            style={{width: '200px', borderRadius: '90px'}}
-                            onClick={onClickButtonHandler}>
-                        Show answer
-                    </Button>
-                    {answer &&
-                        <div className={s.learnBlock}>
-                            <div className={s.titleSection}>Answer:</div>
-                            <div className={s.QuestionAnswerBlock}>
-                                {card.answerImg
-                                    ?
-                                    <img alt={'answer image'}
-                                         src={card.answerImg}
-                                         style={{maxWidth: '375px', maxHeight: '120px', marginTop: '20px'}}/>
-                                    :
-                                    <div>{card.answer}</div>
-                                }
-                            </div>
-                            <div className={s.rateSection}>
-                                <div className={s.titleSection} >Rate yourself:</div>
-                                {grades.map((el, index) => {
-                                    const onClickHandler = () => {
-                                        setValue(index + 1)
+            if (cards.length > 0) {
+                setCard(getCard(cards))
+            }
+
+            return () => {
+            }
+        }, [dispatch, packId, cards])
+
+        return (
+            <div className={f.container}>
+                <BackToPackList/>
+                <div>
+                    <div className={t.titlePack}>Learn: {`'${packName}'`}</div>
+                    <div className={s.learnBlock}>
+                        <p className={s.titleSection}>Question:</p>
+                        <div className={s.QuestionAnswerBlock}>
+                            {card.questionImg
+                                ?
+                                <img alt={'question image'}
+                                     src={card.questionImg}
+                                     className={s.img}/>
+                                :
+                                <div>{card.question}</div>
+                            }
+                        </div>
+                        <p className={f.text}>Number of attempts to answer the question: {card.shots}</p>
+                        <Button variant={'contained'}
+                                className={s.button}
+                                onClick={onClickButtonHandler}>
+                            Show answer
+                        </Button>
+                        {answer &&
+                            <div className={`${s.learnBlock} ${s.rateBlock}`}>
+                                <p className={s.titleSection}>Answer:</p>
+                                <div className={s.QuestionAnswerBlock}>
+                                    {card.answerImg
+                                        ?
+                                        <img alt={'answer image'}
+                                             src={card.answerImg}
+                                             className={s.img}/>
+                                        :
+                                        <div>{card.answer}</div>
                                     }
+                                </div>
+                                <div className={s.rateSection}>
+                                    <p className={s.titleSection}>Rate yourself:</p>
+                                    {grades.map((el, index) => {
+                                        const onClickHandler = () => {
+                                            setValue(index + 1)
+                                        }
 
-                                    return (
-                                        <div key={index}>
-                                            <FormControlLabel
-                                                onClick={onClickHandler}
-                                                value={value}
-                                                checked={value === index + 1}
-                                                control={<Radio/>}
-                                                label={el}/>
-                                        </div>
-                                    )
-                                })}
-                            </div>
-                            <div className={s.button}>
+                                        return (
+                                            <div key={index}>
+                                                <FormControlLabel
+                                                    onClick={onClickHandler}
+                                                    value={value}
+                                                    checked={value === index + 1}
+                                                    control={<Radio/>}
+                                                    label={el}/>
+                                            </div>
+                                        )
+                                    })}
+                                </div>
                                 <Button disabled={!value}
                                         onClick={onNext}
                                         variant='contained'
-                                        style={{width: '200px', borderRadius: '90px'}}>
+                                        className={s.button}>
                                     Next Question
                                 </Button>
                             </div>
-                        </div>
-                    }
+                        }
+                    </div>
                 </div>
             </div>
-        </div>
-    )
-}
+        )
+    }
 )
